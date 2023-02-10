54dc461272002c1d76fa3be31e30201cef08e784
airgedon <104425308+airgedon@users.noreply.github.com>
2023-02-10 12:31:04 +0600

    Update settings.py

--- edu_platform/settings.py
+++ edu_platform/settings.py
@@ -73,8 +73,12 @@ WSGI_APPLICATION = "edu_platform.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'edu_math',
+        'USER': 'hello' ,
+        'PASSWORD': '1',
+        'HOST': 'db',
+        'PORT': 5432
     }
 }
 
